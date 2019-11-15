2dd7485138113bed61e299abc2c17fd725e73353
fardmuhammad <heyitsfard@gmail.com>
2019-11-15 16:19:31 -0600

    Updates

---
+++ precache-manifest.ceee84211358707c3c817de1ee395704.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014358e2fe4a1659c279fa2d8a11df28",
+    "url": "/trench-coat/index.html"
+  },
+  {
+    "revision": "8fd3d7abebe47b3fd280",
+    "url": "/trench-coat/static/css/main.410426db.chunk.css"
+  },
+  {
+    "revision": "67ba3ea387d52a3b0189",
+    "url": "/trench-coat/static/js/2.ceccff29.chunk.js"
+  },
+  {
+    "revision": "8fd3d7abebe47b3fd280",
+    "url": "/trench-coat/static/js/main.08c7e679.chunk.js"
+  },
+  {
+    "revision": "ab083da9a98a4aa4bdea",
+    "url": "/trench-coat/static/js/runtime-main.b8c868e7.js"
+  }
+]);
